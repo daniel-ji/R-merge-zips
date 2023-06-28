c480389926f02ab757eb45fdbb5b9ae3d3d1f171
Daniel Ji <jidaniel1234@gmail.com>
2023-06-28 14:13:44 -0700

    fix cluster identifiers not showing up, implement optparse for arg parsing

--- merge-zip.R
+++ merge-zip.R
@@ -1,5 +1,5 @@
-libs <- c("sf", "r2r")
-lapply(libs, require, character.only = TRUE)
+libs <- c("sf", "r2r", "optparse")
+sapply(libs, library, character.only = TRUE)
 
 # configuration variables (change as needed)
 # Note: the input csv file should have the following format: zip,cluster
@@ -7,31 +7,34 @@ zip_shapefile_input <- "sd-zip/sd-zip.shp"
 zip_clusters_input_csv_file <- "sd-zip/sd-zipcode-clusters.csv"
 zip_merged_shapefile_output <- "sd-zip/sd-zip-merged.shp" # Note: errors if this file already exists
 zip_shapefile_identifier <- "zip"
+draw_plots <- FALSE
+override_existing_output <- FALSE
 
-# TODO: finish
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 0) {
-    cat("\nNo arguments provided. Using values hard-coded into script.\n\n\n")
-} else if (length(args) == 3 || length(args) == 4) {
-    zip_shapefile_input <- args[1]
-    zip_clusters_input_csv_file <- args[2]
-    zip_merged_shapefile_output <- args[3]
-    if (length(args) == 4) {
-        zip_shapefile_identifier <- args[4]
-    }
-} else {
-    cat("\nInvalid number of arguments provided. Usage: Rscript merge-zip.R <zip_shapefile_input> <zip_clusters_input_csv_file> <zip_merged_shapefile_output> [zip_shapefile_identifier]\n\n\n")
-    quit()
-}
+option_list <- list(
+    make_option(c("-i", "--input"), action = "store", type = "character", default = zip_shapefile_input, help = "Input shapefile"),
+    make_option(c("-c", "--clusters"), action = "store", type = "character", default = zip_clusters_input_csv_file, help = "Input csv file with zip -> cluster mapping"),
+    make_option(c("-o", "--output"), action = "store", type = "character", default = zip_merged_shapefile_output, help = "Output shapefile"),
+    make_option(c("-s", "--shapefile-identifier"), action = "store", type = "character", default = zip_shapefile_identifier, help = "Shapefile identifier"),
+    make_option(c("-p", "--draw-plots"), action = "store_true", default = draw_plots, help = "Draw plots"),
+    make_option(c("-r", "--override-existing-output"), action = "store_true", default = override_existing_output, help = "Override existing output")
+)
+
+opt_parser <- parse_args(OptionParser(option_list = option_list))
+
+zip_shapefile_input <- opt_parser$input
+zip_clusters_input_csv_file <- opt_parser$clusters
+zip_merged_shapefile_output <- opt_parser$output
+zip_shapefile_identifier <- opt_parser[["shapefile-identifier"]]
+draw_plots <- opt_parser[["draw-plots"]]
+override_existing_output <- opt_parser[["override-existing-output"]]
 
 # read csv file with zip -> cluster mapping (multiple zips per cluster)
-zip_to_cluster <- read.csv(zip_clusters_input_csv_file, header = TRUE, sep = ",")
+zip_to_cluster <- read.csv(zip_clusters_input_csv_file, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+# change column names
+colnames(zip_to_cluster) <- c("zip", "cluster")
 
 # read shapefiles
 zips <- st_read(zip_shapefile_input)
-
-# change column names
-colnames(zip_to_cluster) <- c("zip", "cluster")
 
 # also create a hashmap frame with cluster -> empty list (but later will be a list of multipolygons)
 unique_clusters <- unique(zip_to_cluster$cluster)
@@ -57,20 +60,22 @@ for (cluster in unique_clusters) {
     # add cluster id to multipolygon
     union_multipolygon <- st_sf(union_multipolygon)
     union_multipolygon$cluster <- cluster
-    union_multipolygon <- st_as_sfc(union_multipolygon)
-    # prevent coordinate system error
-    if (length(merged_multipolygons) == 0) {
-        merged_multipolygons <- union_multipolygon
-    } else {
-        merged_multipolygons <- append(merged_multipolygons, union_multipolygon)
-    }
+    # add to list of multipolygons
+    merged_multipolygons <- append(merged_multipolygons, list(union_multipolygon))
 }
+
+merged_multipolygons <- do.call(rbind, merged_multipolygons)
 
 # merge all the clusters into one multipolygon, but preserve boundaries
 merged_multipolygon <- st_combine(merged_multipolygons)
 
-st_write(merged_multipolygon, zip_merged_shapefile_output)
+if (draw_plots) {
+    plot(st_combine(zips))
+    plot(merged_multipolygon, add = TRUE, border = "red", lwd = 2)
+}
 
-# for sanity check
-# plot(st_combine(zips))
-# plot(merged_multipolygon, add = TRUE, border = "red", lwd = 2)
+if (override_existing_output) {
+    st_write(merged_multipolygon, zip_merged_shapefile_output, append = FALSE)
+} else {
+    st_write(merged_multipolygon, zip_merged_shapefile_output)
+}
